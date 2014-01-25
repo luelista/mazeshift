7716ed35f0e676621ce3d379270bdd948656919a
U-canard\mw <mw@canard.(none)>
2014-01-25 21:28:41 +0100

    map correction

--- levels/map4.lua
+++ levels/map4.lua
@@ -15,8 +15,8 @@ function maphandler:onCollision(direction, mapchar, player, tx, ty, playerIndex)
 end
 
 maphandler.imagemap = {
-   r = { imgStar, player="r", inactiveColor = {111,111,111,111} },
-   b = { imgStar, player="b", inactiveColor = {111,111,111,111} },
+   r = { imgStar, player="r", inactiveColor = {255,255,255,255} },
+   b = { imgStar, player="b", inactiveColor = {255,255,255,255} },
    a = { imgStar,             }
 }
 
