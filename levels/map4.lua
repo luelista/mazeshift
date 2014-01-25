19a95b9b18d7122841ecaed24328324776fa4e5c
Moritz Willig <moritz.willig@gmail.com>
2014-01-25 21:21:12 +0100

    level 4

--- levels/map4.lua
+++ levels/map4.lua
@@ -2,25 +2,28 @@ local maphandler = {}
 
 function maphandler:onCollision(direction, mapchar, player, tx, ty, playerIndex)
    print("onCollision", direction, mapchar, player, tx, ty, playerIndex)
-   
-   if direction == "enter" then
-      labyrinth:credit(100)
-   end
 
+   if mapchar == "a" and direction == "enter" then playerDied("Fall to death") end
+   
+   
    if mapchar == "y" then
       sndBackgroundmusic:pause() sndCredit:play() labyrinth.show_map=true
       setTimeout(function() labyrinth.show_map = false end, 1)
       return " "
    end
+   
 end
 
-maphandler.players = {
-   { player = "yellow", x = 20, y = 3, directionvector = {0,1} }
-}
-
 maphandler.imagemap = {
-   y = { imgStar, player = "y" }
+   r = { imgStar, player="r", inactiveColor = {111,111,111,111} },
+   b = { imgStar, player="b", inactiveColor = {111,111,111,111} },
+   a = { imgStar,             }
 }
 
+maphandler.players = {
+   { player = "red", x = 3, y = 30, directionvector = {1,0} },
+   { player = "yellow", x = 5, y = 3, directionvector = {-1,0} },
+   { player = "blue", x = 90, y = 15, directionvector = {0,-1} },
+}
 
 return maphandler
