1e0e06b235c1da4779ccd80f50838169e30e0769
U-canard\mw <mw@canard.(none)>
2014-01-26 14:26:59 +0100

    map6

--- levels/map6/script.lua
+++ levels/map6/script.lua
@@ -1,19 +1,26 @@
+-- -*- compile-command: "\"c:/Program Files/LOVE/love.exe\" ../.."; -*-
+
 local maphandler = {}
 
 function maphandler:onCollision(direction, mapchar, player, tx, ty, playerIndex)
    
-   if direction == "enter" then
-      labyrinth:credit(100)
+   if direction == "enter" and (mapchar == "r" or mapchar == "y" or mapchar == "b") and player.player == mapchar then
+      sndBackgroundmusic:pause() sndCredit:play() labyrinth:credit(100)
    end
    
 end
 
 maphandler.players = {
-   { player = "blue", x = 2, y = 2, directionvector = {1,1} }
+   { player = "red", x = 9, y = 3, directionvector = {1,0} },
+   { player = "yellow", x = 5, y = 51, directionvector = {0,-1} },
+   { player = "blue", x = 90, y = 3, directionvector = {-1,0} }
 }
 
 maphandler.imagemap = {
-   p = { love.graphics.newImage(levelFolder.."map5_p.png"), consume = true }
+   r = { imgStar, consume = true, player="r", inactiveColor = {111,111,111,111} },
+   y = { imgStar, consume = true, player="y", inactiveColor = {111,111,111,111} },
+   b = { imgStar, consume = true, player="b", inactiveColor = {111,111,111,111} },
+   
 }
 
 
